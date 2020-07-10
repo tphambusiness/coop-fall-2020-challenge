e37c1fbc702094153fb3ecd303999c4ab389c5ba
tphambusiness <tphambusiness@outlook.com>
2020-07-10 14:15:11 -0400

    9/10 cases

--- solution_python.py
+++ solution_python.py
@@ -11,12 +11,12 @@ class EventSourcer():
 
         if (self.currentPoint != len(self.history)):
             # there has been an undo and/or redo command
-            # so this operation overrides everything beyond the current point in history
+            # so this operation overrides the index of the current point
+            # assumed the entire history onwards before and from current point is preserved
+            self.history[self.currentPoint] = num
+        else:
+            self.history.append(num)
 
-            self.history = self.history[:self.currentPoint]
-        
-
-        self.history.append(num)
         self.currentPoint = len(self.history)
 
     def subtract(self, num: int):
@@ -24,23 +24,23 @@ class EventSourcer():
 
         if (self.currentPoint != len(self.history)):
             # there has been an undo and/or redo command
-            # so this operation overrides everything beyond the current point in history
+            # so this operation overrides the index of the current point
+            # assumed the entire history onwards before and from current point is preserved
+            self.history[self.currentPoint] = num * -1
+        else:
+            self.history.append(num * -1) # negated value is added to a list 
 
-            self.history = self.history[:self.currentPoint]
-        
-
-        self.history.append(num * -1) # negated value is added to a list 
         self.currentPoint = len(self.history)
 
     def undo(self):
         if (self.currentPoint > 0): 
-            self.value -= self.history[-1] # take away the last operation done 
+            self.value -= self.history[self.currentPoint-1] # take away the last operation done 
             self.currentPoint -= 1
         
 
     def redo(self):
         if (self.currentPoint < len(self.history)): 
-            self.value += self.history[-1]
+            self.value += self.history[self.currentPoint-1]
             self.currentPoint += 1
         
 
@@ -51,3 +51,15 @@ class EventSourcer():
     def bulk_redo(self, steps: int):
         for i in range(steps):
             self.redo()
+
+e = EventSourcer()
+e.add(5)
+e.add(5)
+e.add(5)
+e.undo()
+e.undo()
+e.add(1)
+for i in e.history:
+    print(i)
+e.redo()
+print(str(e.value) + " end value")
