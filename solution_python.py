df4e01e3cbef58b333494257f0d64c50b4639c1b
tphambusiness <tphambusiness@outlook.com>
2020-07-10 14:34:27 -0400

    truncated testing, 10/10 cases

--- solution_python.py
+++ solution_python.py
@@ -11,39 +11,35 @@ class EventSourcer():
 
         if (self.currentPoint != len(self.history)):
             # there has been an undo and/or redo command
-            # so this operation overrides the index of the current point
+            # so this operation overrides the index of the current point in history
             # assumed the entire history onwards before and from current point is preserved
             self.history[self.currentPoint] = num
         else:
             self.history.append(num)
-
-        self.currentPoint = len(self.history)
+            self.currentPoint = len(self.history)
 
     def subtract(self, num: int):
         self.value -= num
 
         if (self.currentPoint != len(self.history)):
             # there has been an undo and/or redo command
-            # so this operation overrides the index of the current point
+            # so this operation overrides the index of the current point in history
             # assumed the entire history onwards before and from current point is preserved
             self.history[self.currentPoint] = num * -1
         else:
             self.history.append(num * -1) # negated value is added to a list 
-
-        self.currentPoint = len(self.history)
+            self.currentPoint = len(self.history) 
 
     def undo(self):
         if (self.currentPoint > 0): 
             self.value -= self.history[self.currentPoint-1] # take away the last operation done 
             self.currentPoint -= 1
         
-
     def redo(self):
         if (self.currentPoint < len(self.history)): 
-            self.value += self.history[self.currentPoint-1]
+            self.value += self.history[self.currentPoint-1] # reattempt previous operation
             self.currentPoint += 1
         
-
     def bulk_undo(self, steps: int):
         for i in range(steps):
             self.undo()
@@ -51,15 +47,3 @@ class EventSourcer():
     def bulk_redo(self, steps: int):
         for i in range(steps):
             self.redo()
-
-e = EventSourcer()
-e.add(5)
-e.add(5)
-e.add(5)
-e.undo()
-e.undo()
-e.add(1)
-for i in e.history:
-    print(i)
-e.redo()
-print(str(e.value) + " end value")
